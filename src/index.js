bd871186427d6b6a39a69b48d59785a898a50ce6
Duviel Garcia <duviel7@gmail.com>
2021-07-20 13:10:26 -0500

    Project Structure components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { JournalApp } from './JournalApp';
+
+import './styles/styles.scss';
+
 ReactDOM.render(
-    <App />,
+    <JournalApp />,
   document.getElementById('root')
 );
 
